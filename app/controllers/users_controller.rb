d580509adf8a81c1bb54c75b8bc02a2748d6d888
Abner del Cid <abner_jonathan@hotmail.com>
2022-09-12 16:01:15 -0600

    Add admin permissions at controllers and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,9 +44,9 @@ class UsersController < ApplicationController
 
     def destroy
         @user.destroy
-        session[:user_id] = nil
+        session[:user_id] = nil if !current_user.admin?
         flash[:notice] = "Account and all associated articles deleted"
-        redirect_to articles_path
+        redirect_to users_path, status: :see_other
     end
 
     private 
@@ -59,8 +59,8 @@ class UsersController < ApplicationController
     end 
 
     def require_same_user
-        if current_user != @user
-            flash[:alert] = "You can only modify your own profile"
+        if current_user != @user && !current_user.admin?
+            flash[:alert] = "You can only modify or delete your own profile"
             redirect_to @user, status: :see_other
         end
     end
