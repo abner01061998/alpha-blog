2e7a2801149dcee9c5c3b33ceb83376e2601f40f
Abner del Cid <abner_jonathan@hotmail.com>
2022-09-12 14:26:03 -0600

    Validations added to signed in users for articles controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
     helper_method :current_user
     helper_method :logged_in?
     
@@ -9,5 +8,12 @@ class ApplicationController < ActionController::Base
 
     def logged_in?
         !!current_user
+    end
+
+    def require_user
+        if !logged_in?
+            flash[:alert] = "You mus be logged in to perform that action"
+            redirect_to login_path
+        end
     end
 end
