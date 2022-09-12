17f6f08d399676de6a32d29655d81f9b1095b9eb
Abner del Cid <abner_jonathan@hotmail.com>
2022-09-11 20:34:03 -0600

    adding validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
-
+    validates :username, 
+        presence: true, 
+        length: {minimum: 3, maximum:25 }, 
+        uniqueness: { case_sensitive: false }
+        
+    EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, 
+        presence: true, 
+        length: { minimum: 5, maximum:100 }, 
+        uniqueness: { case_sensitive: false },
+        format: { with: EMAIL_REGEXP }
+    
 end
